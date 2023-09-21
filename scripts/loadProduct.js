04c0ccb530f4f6660d3c4d464b75aafd7d137525
Victor Serra B Lemos <serralemosdev@outlook.com>
2023-09-21 11:25:35 -0300

    funcao link pagina produto

--- scripts/loadProduct.js
+++ scripts/loadProduct.js
@@ -1,19 +1,19 @@
 import { catalogo } from "../scripts/data.js";
-
+let produtoID = localStorage.getItem("prodID")
+let produto = catalogo.find((cat) => cat.id == produtoID)
+console.log(produto)
 
 
 const loadProduct = `
 <div class="product_info_image">
-                <img src="${catalogo[0].imagem}" alt="">
+                <img src="${produto.imagem}" alt="">
             </div>
             <div class="product_info">
-                <h2>${catalogo[0].titulo}</h2>
-                <span>R$ ${catalogo[0].preco}</span>
+                <h2>${produto.titulo}</h2>
+                <span>R$ ${produto.preco}</span>
                 <input type="number" name="" id="" value="1">
                 <button>Comprar</button>
-                <p>${catalogo[0].descricao}</p>
-            </div>
-// `
-console.log(catalogo[0])
+                <p>${produto.descricao}</p>
+            </div>`
 
 document.querySelector("#product_container").innerHTML = loadProduct
--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,30 @@
 import { catalogo } from "../scripts/data.js";
 
-let array = ""
+let array = "";
 
-function carregarProdutos (){
-  
+function carregarProdutos() {
   for (const produto of catalogo) {
     const card = `<div class="product_card" id="card-${produto.id}">
-    <a href="" id=${produto.id}>
-        <img class="product_image" src="${produto.imagem}">
+    <a href="produto1.html" alt="" id="${produto.id}">
+        <img class="product_image" src="${produto.imagem}" id="${produto.id}">
     </a>
-    <button id="${produto.id}">clique</button>
-    `
-    document.querySelector("#home_product_grid").innerHTML += card
+        
+    
+    `;
+    document.querySelector("#home_product_grid").innerHTML += card;
   }
 }
-carregarProdutos()
+carregarProdutos();
 
-function carregaProduto(array){
-  let productsCard = document.querySelectorAll(".product_card")
+function carregaProduto(array) {
+  let productsCard = document.querySelectorAll(".product_card");
   for (const productCard of productsCard) {
-    productCard.addEventListener("click" , (e) => 
-    {let produto = e.target.id 
-    array = produto
-  console.log(array)}
-    )
-  } 
-
+    productCard.addEventListener("click", (e) => {
+      let produto = e.target.id;
+      localStorage.setItem("prodID",produto)
+      array = produto;
+      console.log(array);
+    });
+  }
 }
-carregaProduto(array)
-
-
+carregaProduto(array);
